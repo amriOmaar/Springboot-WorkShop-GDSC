ff11b3e13ff2e3319b550720f479b8ea4659539c
Omar AMRI <amri.omar93@gmail.com>
2022-11-16 20:56:41 +0100

    CRUD Classroom done

--- src/main/java/WorkShop_GDSC/esprit/tn/repository/ClassroomRepository.java
+++ src/main/java/WorkShop_GDSC/esprit/tn/repository/ClassroomRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
 
+    Classroom findByName(String name);
 
 }
--- src/main/java/WorkShop_GDSC/esprit/tn/services/classroom/ClassroomService.java
+++ src/main/java/WorkShop_GDSC/esprit/tn/services/classroom/ClassroomService.java
@@ -2,35 +2,50 @@ package WorkShop_GDSC.esprit.tn.services.classroom;
 
 
 import WorkShop_GDSC.esprit.tn.entities.Classroom;
+import WorkShop_GDSC.esprit.tn.repository.ClassroomRepository;
 import WorkShop_GDSC.esprit.tn.services.ICrudService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ClassroomService implements IClassroomService, ICrudService<Classroom, Long> {
+
+
+    @Autowired
+    private ClassroomRepository classroomRepository;
+
     @Override
     public List<Classroom> getAll() {
+        return classroomRepository.findAll();
+    }
+
+    @Override
+    public Classroom add(Classroom classroom) throws Exception {
+        if(classroomRepository.findByName(classroom.getName()) != null)
+            throw new Exception("Classroom already exists");
+        return classroomRepository.save(classroom);
+    }
+
+    @Override
+    public Classroom update(Long id, Classroom classroom) {
+        if(classroomRepository.findById(id).isPresent()){
+            Classroom toUpdateClassroom = classroomRepository.findById(id).get();
+            toUpdateClassroom.setName(classroom.getName());
+            return classroomRepository.save(toUpdateClassroom);
+        }
         return null;
     }
 
     @Override
-    public Classroom add(Classroom class1) throws Exception {
-        return null;
-    }
-
-    @Override
-    public Classroom update(Long aLong, Classroom class1) throws Exception {
-        return null;
-    }
-
-    @Override
-    public void delete(Long aLong) {
-
+    public void delete(Long id) {
+        classroomRepository.deleteById(id);
     }
 
     @Override
     public Classroom findById(Long id) {
-        return null;
+        return classroomRepository.findById(id).get();
     }
+
 }
